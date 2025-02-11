5a7109e4ad53a2c6eff1db319ef60d40a041d49f
insoft <developer@insoft.uk>
2025-02-11 19:10:32 +0000

    Update

--- examples/example.pp
+++ examples/example.pp
@@ -6,9 +6,16 @@ begin
   TEXTOUT_P("Copyright (c) 2023-2025 Insoft. All rights reserved.", 0, 0);
 end;
 
+#PYTHON
+#END
+
 EXPORT START()
 BEGIN
   displayCopyright();
+#PPL
+  // In P+ `=` is treated as `:=` were in PPL `=` is treated as `==`
+  // So only PPL code in this section.
+#END
   WAIT;
   var a:alpha = 0;
   alpha += 10;
