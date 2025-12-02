4d3da892c1aad2d1fb99772e143e47e8901d75f6
insoft <developer@insoft.uk>
2025-12-02 16:53:22 +0000

    Update

--- version_code.h
+++ version_code.h
@@ -21,9 +21,9 @@
 // SOFTWARE.
 
 #define VERSION_NUMBER        "4.0.6"
-#define VERSION_CODE          "D0G04-25L1"
-#define NUMERIC_BUILD          40604
+#define VERSION_CODE          "D0G05-25L1"
+#define NUMERIC_BUILD          40605
 #define BUNDLE_VERSION        "20251202"
-#define INTERNAL_BUILD_CODE   "D0G04"
+#define INTERNAL_BUILD_CODE   "D0G05"
 #define DATE                  "2025 December 02"
 #define YEAR                  "2025"
