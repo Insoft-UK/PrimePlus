329796483258d8bf4aa3ba01cd1b479682e1d656
insoft <developer@insoft.uk>
2025-12-23 18:09:35 +0000

    Update

--- Installer for Xprime/pkgbuild.sh
+++ Installer for Xprime/pkgbuild.sh
@@ -2,34 +2,45 @@
 
 # Your AppleID, TeamID, Password and Name (An app-specific password NOT! AppleID password)
 if [ -z "$APPLE_ID" ]; then
-    source ../notarization.sh
+    source notarization.sh
 fi
 
 if [ -z "$PROJECT_NAME" ]; then
     PROJECT_NAME=package
 fi
 
-# Xprime
+# Executable
+cp ../build/ppl+ package-root/Applications/HP/Xprime.app/Contents/Resources/Developer/usr/bin/ppl+
+
+# Package
 pkgbuild --root package-root \
          --identifier uk.insoft.$PROJECT_NAME \
          --version 1.5 --install-location / \
          $PROJECT_NAME.pkg
          
-productsign --sign "Developer ID Installer: $YOUR_NAME ($TEAM_ID)" $PROJECT_NAME.pkg $PROJECT_NAME-signed.pkg
+# Ask the user a question
+result=$(osascript -e 'display dialog "Do you want to sign the package?" buttons {"Yes", "No"} default button "Yes"')
 
-xcrun notarytool submit --apple-id $APPLE_ID \
-                        --password $PASSWORD \
-                        --team-id $TEAM_ID \
-                        --wait $PROJECT_NAME-signed.pkg
+# Process the result
+if [[ "$result" == *"Yes"* ]]; then
+    productsign --sign "Developer ID Installer: $YOUR_NAME ($TEAM_ID)" $PROJECT_NAME.pkg $PROJECT_NAME-signed.pkg
+
+    xcrun notarytool submit --apple-id $APPLE_ID \
+                            --password $PASSWORD \
+                            --team-id $TEAM_ID \
+                            --wait $PROJECT_NAME-signed.pkg
                         
-# Staple
-xcrun stapler staple $PROJECT_NAME-signed.pkg
+    # Staple
+    xcrun stapler staple $PROJECT_NAME-signed.pkg
 
-# Verify
-xcrun stapler validate $PROJECT_NAME-signed.pkg
+    # Verify
+    xcrun stapler validate $PROJECT_NAME-signed.pkg
 
 # Gatekeeper
-spctl --assess --type install --verbose $PROJECT_NAME-signed.pkg
+    spctl --assess --type install --verbose $PROJECT_NAME-signed.pkg
 
-rm $PROJECT_NAME.pkg
-mv $PROJECT_NAME-signed.pkg ../$PROJECT_NAME\_$(date +"%Y%m%d").pkg
+    rm $PROJECT_NAME.pkg
+    mv $PROJECT_NAME-signed.pkg ../ppl+\_Xprime\_$(date +"%Y%m%d").pkg
+else
+    mv $PROJECT_NAME.pkg ../ppl+\_Xprime\_$(date +"%Y%m%d").pkg
+fi
--- Makefile
+++ Makefile
@@ -5,8 +5,11 @@ all:
 	mkdir -p $(BUILD)
 	g++ -arch x86_64 -arch arm64 -std=c++23 src/*.cpp -o $(BUILD)/$(PROJECT_NAME) -Os -fno-ident -fno-asynchronous-unwind-tables -Wl,-dead_strip -Wl,-x
 	
+clean:
+	rm -rf $(BUILD)/*
+	
 install:
 	cp $(BUILD)/$(PROJECT_NAME) /usr/local/bin/$(PROJECT_NAME)
 	
-clean:
+uninstall:
 	rm /usr/local/bin/$(PROJECT_NAME)
